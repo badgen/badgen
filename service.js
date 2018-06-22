2f7bc350ee43a1911df506ac526f1c4730548530
Amio <amio.cn@gmail.com>
2018-06-23 00:04:30 +0800

    Clean service.js

--- service.js
+++ service.js
@@ -1,19 +1,19 @@
 const http = require('http')
 const router = require('find-my-way')()
 const badgen = require('.')
+const readme = require('fs').readFileSync('./README.md', 'utf-8')
 
-function generate (req, res, params) {
+function serveBadge (req, res, params) {
   res.writeHead(200, { 'Content-Type': 'image/svg+xml;charset=utf-8' })
   res.end(badgen(params))
 }
 
-const readme = require('fs').readFileSync('./README.md', 'utf-8')
 function serveReadme (req, res) {
   res.end(readme)
 }
 
-router.get('/badge/:subject/:status', generate)
-router.get('/badge/:subject/:status/:color', generate)
+router.get('/badge/:subject/:status', serveBadge)
+router.get('/badge/:subject/:status/:color', serveBadge)
 router.get('/', serveReadme)
 
 const server = http.createServer((req, res) => router.lookup(req, res))
