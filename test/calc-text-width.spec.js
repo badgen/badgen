3b0914e5d2be90cb8826d31536c7c1366e270e2a
Amio <amio.cn@gmail.com>
2018-06-29 21:40:55 +0800

    Update test

--- test/calc-text-width.spec.js
+++ test/calc-text-width.spec.js
@@ -5,5 +5,6 @@ tap.test('calc width for "npm"', t => {
   t.ok(typeof Arial12 === 'function', 'export function: Arial12')
   t.ok(Number.isFinite(Arial12('npm')), 'result is number')
   t.ok(Arial12('npm') > 0, 'result is positive number')
+  t.ok(Arial12('npm') === 24.543750000000003, 'result is correct value')
   t.end()
 })
