aeadb70a85756af8511bfc76c7b7e067c4a491ae
Amio <amio.cn@gmail.com>
2018-07-11 13:36:30 +0800

    Limit widthTable precision to 2

--- test/calc-text-width.spec.js
+++ test/calc-text-width.spec.js
@@ -2,8 +2,8 @@ const tap = require('tap')
 const calcWidth = require('../lib/calc-text-width.js').Verdana11
 
 tap.test('calc width for "npm"', t => {
-  t.ok(typeof calcWidth === 'function', 'export function: Verdana12')
+  t.ok(typeof calcWidth === 'function', 'export calcWidth function')
   t.ok(Number.isFinite(calcWidth('npm')), 'result is a number')
-  t.is(calcWidth('npm'), 24.51, 'result is correct value')
+  t.is(calcWidth('npm'), 24.9, 'result is correct value')
   t.end()
 })
