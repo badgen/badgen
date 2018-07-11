05d3fabf5bebadb8a90a917dd834ac75348ac606
Amio <amio.cn@gmail.com>
2018-07-11 11:03:51 +0800

    Add preview page for debuging

---
+++ test/PREVIEW.md
@@ -0,0 +1,21 @@
+# Badgen Preview
+
+## Colors
+
+![](http://localhost:3000/color/blue/blue)
+![](http://localhost:3000/color/cyan/cyan)
+![](http://localhost:3000/color/green/green)
+![](http://localhost:3000/color/yellow/yellow)
+![](http://localhost:3000/color/orange/orange)
+![](http://localhost:3000/color/red/red)
+![](http://localhost:3000/color/pink/pink)
+![](http://localhost:3000/color/purple/purple)
+![](http://localhost:3000/color/grey/grey)
+
+## Badges
+
+| Badge | URL |
+| --- | --- |
+|![](http://localhost:3000/chat/gitter/purple) | http://localhost:3000/chat/gitter/purple |
+|![](http://localhost:3000/style/standard/f2a) | http://localhost:3000/style/standard/f2a |
+|![](http://localhost:3000/license/Apache-2.0/blue) | http://localhost:3000/license/Apache-2.0/blue |
--- test/serve.js
+++ test/serve.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const path = require('path')
+const serveMarked = require('serve-marked')
 const badgen = require('..')
 
 // @example
@@ -9,4 +11,27 @@ const serveBadge = (req, res) => {
   res.end(badgen({subject, status, color}))
 }
 
-http.createServer(serveBadge).listen(3000)
+// @example
+// http://localhost:3000
+const md = path.join(__dirname, 'PREVIEW.md')
+const serveIndex = serveMarked(md, {
+  title: 'Badgen - Fast badge generator',
+  preset: 'merri',
+  inlineCSS: `
+    body { color: #333 }
+    a { text-decoration: none; color: #06D }
+    a:hover { text-decoration: underline }
+    table { border-spacing: 0 }
+    td { padding: 0 1em 0 0; height: 24px; font: 14px/14px sans-serif }
+    td a { font: 14px/14px monospace; vertical-align: top }
+  `
+})
+
+http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      return serveIndex(req, res)
+    default:
+      return serveBadge(req, res)
+  }
+}).listen(3000)
