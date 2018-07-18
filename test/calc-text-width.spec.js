4dfe3501b2428f2f323918086acb0a7cc1b2c3e5
Amio <amio.cn@gmail.com>
2018-07-18 14:11:11 +0800

    Add test coverage report

--- test/calc-text-width.spec.js
+++ test/calc-text-width.spec.js
@@ -1,10 +1,20 @@
 const tap = require('tap')
 const calcWidth = require('../lib/calc-text-width.js').Verdana11
 
-tap.test('calc width for "npm"', t => {
+tap.test('basic functions', t => {
   t.ok(typeof calcWidth === 'function', 'export calcWidth function')
-  t.ok(Number.isFinite(calcWidth('npm')), 'result is a number')
+  t.ok(Number.isFinite(calcWidth('')), 'result is a number')
+  t.ok(calcWidth('') === 0, 'return 0 for empty string')
+  t.end()
+})
+
+tap.test('calc width for "npm"', t => {
   t.is(calcWidth('npm'), 24.9, 'result is correct value')
+  t.end()
+})
+
+tap.test('calc width for unicode', t => {
+  t.is(calcWidth('壹佰贰拾叁'), 55, 'result is correct value')
   t.end()
 })
 
