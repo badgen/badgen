01c87f54c4a83593af96dd0599a031f5c8d5e681
Wei He <git@weispot.com>
2018-07-19 01:46:08 -0400

    Add snapshot tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,23 +4,27 @@ const badgen = require('..')
 tap.test('generate badge with { subject, status }', t => {
   const svg = badgen({ subject: 'npm', status: 'v1.0.0' })
   t.ok(typeof svg === 'string', 'successfully generated')
+  t.matchSnapshot(svg, 'snapshot')
   t.end()
 })
 
 tap.test('generate badge with { subject, status, color }', t => {
   const svg = badgen({ subject: 'npm', status: 'v1.0.0', color: 'ADF' })
   t.ok(typeof svg === 'string', 'successfully generated')
+  t.matchSnapshot(svg, 'snapshot')
   t.end()
 })
 
 tap.test('generate badge with { subject, status, style }', t => {
   const svg = badgen({ subject: 'npm', status: 'v1.0.0', style: 'flat' })
   t.ok(typeof svg === 'string', 'successfully generated')
+  t.matchSnapshot(svg, 'snapshot')
   t.end()
 })
 
 tap.test('generate badge with { subject, status, color, style }', t => {
   const svg = badgen({ subject: 'npm', status: 'v1.0.0', color: 'ADF', style: 'flat' })
   t.ok(typeof svg === 'string', 'successfully generated')
+  t.matchSnapshot(svg, 'snapshot')
   t.end()
 })
