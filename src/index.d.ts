e8afa7383935ce6eddd9369924cf2dbe7dff5c55
Amio <amio.cn@gmail.com>
2019-01-29 21:11:39 +0800

    add index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+interface BadgenOptions {
+  status: string;
+  subject?: string;
+  color?: string;
+  style?: StyleOption;
+  icon?: string;
+  iconWidth?: 13;
+}
+
+type StyleOption = 'flat' | 'classic'
+
+export default function badgen(options: BadgenOptions): string;
