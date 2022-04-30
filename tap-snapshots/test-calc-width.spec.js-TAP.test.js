18f79c98b8ec4b04fa48dc3c212018ce9055aa20
Amio <amio.cn@gmail.com>
2022-04-30 17:05:10 +0800

    chore: bump esbuild to 0.14, tap to 16

--- tap-snapshots/test-calc-width.spec.js-TAP.test.js
+++ tap-snapshots/test-calc-width.spec.js-TAP.test.js
@@ -5,18 +5,18 @@
  * Make sure to inspect the output below.  Do not ignore changes!
  */
 'use strict'
-exports[`test/calc-width.spec.js TAP calc width for "npm" > result is correct 1`] = `
+exports[`test/calc-width.spec.ts TAP calc width for "npm" > result is correct 1`] = `
 249
 `
 
-exports[`test/calc-width.spec.js TAP calc width for emojis > result is correct 1`] = `
+exports[`test/calc-width.spec.ts TAP calc width for emojis > result is correct 1`] = `
 330
 `
 
-exports[`test/calc-width.spec.js TAP calc width for special chars > result is correct 1`] = `
+exports[`test/calc-width.spec.ts TAP calc width for special chars > result is correct 1`] = `
 600
 `
 
-exports[`test/calc-width.spec.js TAP calc width for unicode > result is correct 1`] = `
+exports[`test/calc-width.spec.ts TAP calc width for unicode > result is correct 1`] = `
 550
 `
